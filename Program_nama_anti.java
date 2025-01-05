31049923084cb30cec52318628026739860cc08f
luthfi303 <priantiluthfi@gmail.com>
2025-01-05 11:44:39 +0700

    Add files via upload

---
+++ Program_nama_anti.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package program_nama_anti;
+
+/**
+ *
+ * @author PRIANTI MARATUL LUTH
+ */
+public class Program_nama_anti {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Nama saya Anti !");
+        System.out.print("Welcome to Java !");
+    }
+    
+}
